317b2f1ee1f2aa8d905317fde9bb802385947869
Armel Siewe <asiewe@outlook.de>
2025-02-06 17:11:49 +0100

    filter and search

--- webapp/controller/Overview.controller.ts
+++ webapp/controller/Overview.controller.ts
@@ -3,6 +3,11 @@ import BaseController from "./BaseController";
 import Input from "sap/m/Input";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import { Todo } from "todoApp/model";
+import SearchField from "sap/m/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
+import SegmentedButton from "sap/m/SegmentedButton";
 
 /**
  * @namespace todoApp.controller
@@ -42,6 +47,55 @@ export default class Overview extends BaseController {
       );
       todos.todos.splice(todoIndex, 1);
       todoModel.setData(todos);
+    }
+  }
+
+  onSearchFieldLiveChange(event: Event) {
+    const searchField: SearchField = event.getSource();
+    const searchValue = searchField.getValue();
+    const searchFilter = [];
+    if (searchValue && searchValue.length > 0) {
+      searchFilter.push(
+        new Filter("todo", FilterOperator.Contains, searchValue)
+      );
+    }
+    this.applySearchFilter(searchFilter);
+  }
+
+  onSegmentedButtonSelectionChange(event: Event) {
+    const segmentedButton = event.getSource() as SegmentedButton;
+    const filterKey = segmentedButton.getSelectedKey();
+    const filters: Filter[] = [];
+
+    // eslint-disable-line default-case
+    switch (filterKey) {
+      case "active":
+        filters.push(new Filter("completed", FilterOperator.EQ, false));
+        break;
+      case "completed":
+        filters.push(new Filter("completed", FilterOperator.EQ, true));
+        break;
+      case "all":
+      default:
+      // Don't use any filter
+    }
+    this.applySearchFilter(filters);
+  }
+
+  onButtonClearCompletedPress() {
+    const todoModel = this.getTodoModel();
+    if (todoModel) {
+      const todos = todoModel.getData();
+      const activeTodos = todos.todos.filter((todo: Todo) => !todo.completed);
+      todoModel.setData({ todos: activeTodos });
+    }
+  }
+
+  private applySearchFilter(searchFilter: Filter[]) {
+    const list = this.byId("idTodosList");
+    if (list) {
+      const binding = list.getBinding("items") as ListBinding;
+      binding.filter(searchFilter);
     }
   }
 
