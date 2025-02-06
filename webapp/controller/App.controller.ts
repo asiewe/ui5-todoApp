d03643ca83c4a7d04b7aaa464e9cc14758b40837
Armel Siewe <asiewe@outlook.de>
2025-02-06 21:17:14 +0100

    home page

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,23 @@
+import JSONModel from "sap/ui/model/json/JSONModel";
 import BaseController from "./BaseController";
+import SideNavigation from "sap/tnt/SideNavigation";
 
 /**
  * @namespace todoApp.controller
  */
-export default class App extends BaseController {}
+export default class App extends BaseController {
+  /**
+   * Handles the press event of the navigation bar toggle button.
+   * Toggles the expanded state of the side navigation.
+   *
+   * @returns {void}
+   */
+  onNavBartoggleButtonPress() {
+    const sideNavigation = this.byId("idAppSideNavigation") as SideNavigation;
+    sideNavigation.setExpanded(!sideNavigation.getExpanded());
+  }
+
+  private getAppStateModel() {
+    return this.getModel("AppState") as JSONModel | undefined;
+  }
+}
--- webapp/model/index.ts
+++ webapp/model/index.ts
@@ -1,6 +1,31 @@
+/**
+ * Represents a Todo item.
+ */
 export interface Todo {
+  /**
+   * The unique identifier of the Todo item.
+   */
   id: number;
+
+  /**
+   * Indicates whether the Todo item is completed.
+   */
   completed: boolean;
+
+  /**
+   * The description or title of the Todo item.
+   */
   todo: string;
+
+  /**
+   * The unique identifier of the user who created the Todo item.
+   */
   userId: number;
+}
+
+/**
+ * Represents the state of the application.
+ */
+export interface AppState {
+  githubUrl: string;
 }
